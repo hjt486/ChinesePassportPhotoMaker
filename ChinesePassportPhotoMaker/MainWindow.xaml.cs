50cb1bd50eb42823ce941a6cc9b27047eff001ce
hjt486 <hjt486@gmail.com>
2021-10-12 16:11:19 -0500

    Overlay Added
    Added Overlay, new example photo, changed the name of controller class

--- ChinesePassportPhotoMaker/MainWindow.xaml.cs
+++ ChinesePassportPhotoMaker/MainWindow.xaml.cs
@@ -15,61 +15,80 @@ using System.Windows.Shapes;
 
 namespace ChinesePassportPhotoMaker
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    private ObjectManipulationControl _imageViewerControl = new ObjectManipulationControl();
+    private ObjectManipulationControl _overlayFloatingViewerControl = new ObjectManipulationControl();
+
+    public MainWindow()
     {
-        private ImageViewerControl _imageViewerControl = new ImageViewerControl();
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-        private void ImageViewer_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            _imageViewerControl.SetCoordsMouseDown(
-                Mouse.GetPosition(Application.Current.MainWindow).X,
-                Mouse.GetPosition(Application.Current.MainWindow).Y);
-            DebugLabel1.Content = Mouse.GetPosition(Application.Current.MainWindow).X.ToString();
-            DebugLabel2.Content = Mouse.GetPosition(Application.Current.MainWindow).Y.ToString();
-        }
-
-        private void ImageViewer_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            _imageViewerControl.SetCoordsMouseUp(
-                Mouse.GetPosition(Application.Current.MainWindow).X,
-                Mouse.GetPosition(Application.Current.MainWindow).Y);
-            DebugLabel1.Content = Mouse.GetPosition(Application.Current.MainWindow).X.ToString();
-            DebugLabel2.Content = Mouse.GetPosition(Application.Current.MainWindow).Y.ToString();
-        }
-
-        private void ImageViewer_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                _imageViewerControl.SetCoordsMouseInMoving(
-                Mouse.GetPosition(Application.Current.MainWindow).X,
-                Mouse.GetPosition(Application.Current.MainWindow).Y);
-                Canvas.SetLeft(ImageViewer, _imageViewerControl.GetCoordsX());
-                Canvas.SetTop(ImageViewer, _imageViewerControl.GetCoordsY());
-                DebugLabel1.Content = Mouse.GetPosition(Application.Current.MainWindow).X.ToString();
-                DebugLabel2.Content = Mouse.GetPosition(Application.Current.MainWindow).Y.ToString();
-            }
-        }
-
-        private void ImageViewer_MouseWheel(object sender, MouseWheelEventArgs e)
-        {
-            if (e.Delta > 0)
-            {
-                _imageViewerControl.SetImageByPoint(2);
-            }
-            else if (e.Delta < 0)
-            {
-                _imageViewerControl.SetImageByPoint(-3);
-            }
-            ImageViewer.Width = _imageViewerControl.ImageWidth;
-            ImageViewer.Height = _imageViewerControl.ImageHeight;
-        }
+      InitializeComponent();
     }
+
+    private void ImageViewer_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+      _imageViewerControl.IsSelected = true;
+      _imageViewerControl.SetCoordsMouseDown(
+          Mouse.GetPosition(Application.Current.MainWindow).X,
+          Mouse.GetPosition(Application.Current.MainWindow).Y);
+      //DebugLabel1.Content = Mouse.GetPosition(Application.Current.MainWindow).X.ToString();
+      //DebugLabel2.Content = Mouse.GetPosition(Application.Current.MainWindow).Y.ToString();
+    }
+
+    private void ImageViewer_MouseUp(object sender, MouseButtonEventArgs e)
+    {
+      _imageViewerControl.SetCoordsMouseUp();
+      //DebugLabel1.Content = Mouse.GetPosition(Application.Current.MainWindow).X.ToString();
+      //DebugLabel2.Content = Mouse.GetPosition(Application.Current.MainWindow).Y.ToString();
+      _imageViewerControl.IsSelected = false;
+    }
+
+    private void ImageOrOverlay_MouseMove(object sender, MouseEventArgs e)
+    {
+      if (_imageViewerControl.IsSelected && e.LeftButton == MouseButtonState.Pressed)
+      {
+        _imageViewerControl.SetCoordsMouseInMoving(
+        Mouse.GetPosition(Application.Current.MainWindow).X,
+        Mouse.GetPosition(Application.Current.MainWindow).Y);
+        Canvas.SetLeft(ImageViewer, _imageViewerControl.GetCoordsX());
+        Canvas.SetTop(ImageViewer, _imageViewerControl.GetCoordsY());
+        //DebugLabel1.Content = Mouse.GetPosition(Application.Current.MainWindow).X.ToString();
+        //DebugLabel2.Content = Mouse.GetPosition(Application.Current.MainWindow).Y.ToString();
+      }
+      else if (_overlayFloatingViewerControl.IsSelected && e.LeftButton == MouseButtonState.Pressed)
+      {
+        _overlayFloatingViewerControl.CoordMouseInMovingY = Mouse.GetPosition(Application.Current.MainWindow).Y;
+        Canvas.SetTop(OverlayFloating, _overlayFloatingViewerControl.GetCoordsY());
+      }
+    }
+
+    private void ImageViewer_MouseWheel(object sender, MouseWheelEventArgs e)
+    {
+      if (e.Delta > 0)
+      {
+        _imageViewerControl.SetImageByPoint(2);
+      }
+      else if (e.Delta < 0)
+      {
+        _imageViewerControl.SetImageByPoint(-3);
+      }
+      ImageViewer.Width = _imageViewerControl.ImageWidth;
+      ImageViewer.Height = _imageViewerControl.ImageHeight;
+    }
+
+    private void OverlayFloating_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+      _overlayFloatingViewerControl.IsSelected = true;
+      _overlayFloatingViewerControl.CoordMouseDownY = Mouse.GetPosition(Application.Current.MainWindow).Y;
+    }
+
+    private void OverlayFloating_MouseUp(object sender, MouseButtonEventArgs e)
+    {
+      _overlayFloatingViewerControl.SetCoordsMouseUpY();
+      _overlayFloatingViewerControl.IsSelected = false;
+    }
+  }
 }
---
+++ ChinesePassportPhotoMaker/ObjectManipulationControl.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChinesePassportPhotoMaker
+{
+  class ObjectManipulationControl
+  {
+    private double _canvasX = 0;
+    private double _canvasY = 0;
+    private double _coordMouseDownX = 0;
+    private double _coordMouseDownY = 0;
+    private double _coordMouseInMovingX = 0;
+    private double _coordMouseInMovingY = 0;
+    private double _imageWidth = 600;
+    private double _imageHeight = 800;
+    private double _imageWidthByHeightRatio = 0.75;
+    private bool _isSelected = false;
+    public double ImageWidth
+    {
+      get { return _imageWidth; }
+      set { _imageWidth = value; }
+    }
+    public double ImageHeight
+    {
+      get { return _imageHeight; }
+      set { _imageHeight = value; }
+    }
+    public double ImageWidthByHeightRatio
+    {
+      get { return _imageWidthByHeightRatio; }
+      set { _imageWidthByHeightRatio = value; }
+    }
+    public double CoordMouseDownX
+    {
+      get { return _coordMouseDownX; }
+      set { _coordMouseDownX = value; }
+    }
+    public double CoordMouseDownY
+    {
+      get { return _coordMouseDownY; }
+      set { _coordMouseDownY = value; }
+    }
+    public double CoordMouseInMovingX
+    {
+      get { return _coordMouseInMovingX; }
+      set { _coordMouseInMovingX = value; }
+    }
+    public double CoordMouseInMovingY
+    {
+      get { return _coordMouseInMovingY; }
+      set { _coordMouseInMovingY = value; }
+    }
+    public bool IsSelected
+    {
+      get { return _isSelected; }
+      set { _isSelected = value; }
+    }
+    public void SetCoordsMouseDown(double xCoord, double yCoord)
+    {
+      _coordMouseDownX = xCoord;
+      _coordMouseDownY = yCoord;
+    }
+    public void SetCoordsMouseInMoving(double xCoord, double yCoord)
+    {
+      _coordMouseInMovingX = xCoord;
+      _coordMouseInMovingY = yCoord;
+    }
+    public void SetCoordsMouseUp()
+    {
+      _canvasX += _coordMouseInMovingX - _coordMouseDownX;
+      _canvasY += _coordMouseInMovingY - _coordMouseDownY;
+    }
+    public void SetCoordsMouseUpY()
+    {
+      _canvasY += _coordMouseInMovingY - _coordMouseDownY;
+    }
+    public double GetCoordsX()
+    {
+      return _canvasX + _coordMouseInMovingX - _coordMouseDownX;
+    }
+    public double GetCoordsY()
+    {
+      return _canvasY + _coordMouseInMovingY - _coordMouseDownY;
+    }
+    public void SetImageByPoint(int pointDeltaPercent)
+    {
+      _imageWidth = _imageWidth * (100 + pointDeltaPercent) / 100;
+      _imageHeight = _imageWidth / _imageWidthByHeightRatio;
+    }
+
+
+  }
+}
